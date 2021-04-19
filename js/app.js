481eab4d5b7c7c0bc2336c0778aabccd7a5d3871
Matheus <matheus081105@gmail.com>
2021-04-19 09:26:18 -0300

    .

--- js/app.js
+++ js/app.js
@@ -13,7 +13,19 @@ $(function(){
     var place = $('.back-to-top-location')
 
     $(topo).click(function() {
-        $('html,body').animate({scrollTop:$(place).offset().top})
+        $('html,body').animate({
+            scrollTop:$(place).offset().top
+        })
         return false;
+    })
+
+    //-----------------------------------------//
+
+    $(window).scroll(function(){
+        let value = window.scrollY
+        $('.chamada1').css('position', 'relative')
+        $('.chamada1').css('right', value * 1.2 + 'px')
+        $('.chamada1').css('top', value * 0.2 + 'px')
+        console.log(value)
     })
 })
--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,9 @@ $(function() {
     $('nav.desktop ul li a,nav.mobile ul li a').click(function(){
         var href = $(this).attr('href'); 
 
-        $('html,body').animate({scrollTop:$(href).offset().top});
+        $('html,body').animate({
+            scrollTop:$(href).offset().top
+        });
 
         return false;
     })
